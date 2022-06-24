e1b9100db47fc7e036695873f01925d3980531cf
tarajb <tarajbhardwaj@yahoo.in>
2022-06-24 23:40:56 +0530

    component directives

--- src/app/myComponents/child.component.ts
+++ src/app/myComponents/child.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-child',
   template:
-    '<h1 [ngClass]="currentClasses" [ngStyle]="currentStyle"> {{name}}</h1> <ng-content></ng-content> {{today | date}}',
+    '<h1 [ngClass]="currentClasses" [ngStyle]="currentStyle"> {{name}}</h1> <ng-content></ng-content> {{today | date}} <br><label for="example">{{name}}</label><br> <input [(ngModel)]="name" id="example"/>',
   styles: [
     `
   h1: {
